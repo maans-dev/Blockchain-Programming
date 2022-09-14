b2157c52aea5a62cd7468a53d34c68a96e837914
Herman Rademeyer <98677997+maans-dev@users.noreply.github.com>
2022-09-14 14:14:41 +0200

    Update Program.cs

--- blockchain-programming/Program.cs
+++ blockchain-programming/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using NBitcoin;
 
 namespace _125350929
@@ -8,14 +8,20 @@ namespace _125350929
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! " + new Key().GetWif(Network.Main));
+            var publicKeyHash = new KeyId("14836dbe7f38c5ac3d49e8d790af808a4ee9edcf");
+
+            var testNetAddress = publicKeyHash.GetAddress(Network.TestNet);
+            var mainNetAddress = publicKeyHash.GetAddress(Network.Main);
+
+            Console.WriteLine(mainNetAddress.ScriptPubKey);         
         }
     }
 }
 
-//Generating a public key with NBitcoin
+//Generating a private key with NBitcoin, used to send bitcoins
 // Key privateKey = new Key();
 //One-way Cryptographic function to generate a public key 
 // Pubkey publickey = privateKey.Pubkey
-//You can now acquire your Bitcoin Address from your public key
+//You can now acquire your Bitcoin Address from your public key and network to be used.
 // Console.WriteLine(publicKey.GetAddress(ScriptPubKeyType.Legacy, Network.Main)); // 1PUYsjwfNmX64wS368ZR5FMouTtUmvtmTY
 // Console.WriteLine(publicKey.GetAddress(ScriptPubKeyType.Legacy, Network.TestNet)
