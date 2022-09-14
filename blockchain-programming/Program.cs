258e51290b60047e5dc327e237c8d7d812e229ec
Herman Rademeyer <98677997+maans-dev@users.noreply.github.com>
2022-09-14 11:41:44 +0200

    Update Program.cs

--- blockchain-programming/Program.cs
+++ blockchain-programming/Program.cs
@@ -11,3 +11,11 @@ namespace _125350929
         }
     }
 }
+
+//Generating a public key with NBitcoin
+// Key privateKey = new Key();
+//One-way Cryptographic function to generate a public key 
+// Pubkey publickey = privateKey.Pubkey
+//You can now acquire your Bitcoin Address from your public key
+// Console.WriteLine(publicKey.GetAddress(ScriptPubKeyType.Legacy, Network.Main)); // 1PUYsjwfNmX64wS368ZR5FMouTtUmvtmTY
+// Console.WriteLine(publicKey.GetAddress(ScriptPubKeyType.Legacy, Network.TestNet)
